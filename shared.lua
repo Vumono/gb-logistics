ba87a65c975fe698d9fefecc3ed61d3e6562885a
Vumono <87247208+Vumono@users.noreply.github.com>
2022-01-09 16:37:59 +0100

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Payout = 500
+Config.DeliveryLocations = {
+    --vector3(x,y,z), -- Use this as a template if needed
+    vector3(28.2090, -1340.4457, 29.4970), -- Coords of locations to start delivering 
+}
