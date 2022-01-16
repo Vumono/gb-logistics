d43ee19434be75ff6c715e65ae2dcc2b1d2123a5
Vumono <87247208+Vumono@users.noreply.github.com>
2022-01-16 16:17:51 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,16 @@
 RegisterNetEvent('gb-logistics:Payment')
 AddEventHandler('gb-logistics:Payment', function(payment)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.addMoney(payment)
+    if payment ~= nil then
+        local amount = Config.moneypermeter*payment
+        if amount <= 10000 then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            if xPlayer ~= nil then
+                xPlayer.addMoney(amount)
+            end
+        else
+            print(xPlayer 'hackeroo')
+
+        end
+    end
 
 end)
